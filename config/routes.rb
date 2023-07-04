9d16ffb98e8997de202047f7a7d162ec979b9266
Ayomipo Majek <ayomipomajek@gmail.com>
2023-07-04 18:13:07 +0100

    food list worked on

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'foods#index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  resources :foods, only: [:index, :new, :create, :destroy]
 
   # Defines the root path route ("/")
   # root "articles#index"
