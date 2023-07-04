d18d2cd4d9c2dff6e0c8e0b0ebc4f43a13d78a84
rmrmrmr <ricardomartineza77@gmail.com>
2023-07-04 10:41:55 -0600

    Correct linter errors

--- app/controllers/publicrecipes_controller.rb
+++ app/controllers/publicrecipes_controller.rb
@@ -1,4 +1,3 @@
 class PublicrecipesController < ApplicationController
-  def index
-  end
+  def index; end
 end
--- app/controllers/shoppinglist_controller.rb
+++ app/controllers/shoppinglist_controller.rb
@@ -1,4 +1,3 @@
 class ShoppinglistController < ApplicationController
-  def index
-  end
+  def index; end
 end
