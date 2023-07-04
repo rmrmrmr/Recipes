0def2f859c4b46b6f354eb678f3a43f3331e9134
rmrmrmr <ricardomartineza77@gmail.com>
2023-07-04 11:46:12 -0600

    Correct typo on migrations

--- db/migrate/20230704153934_create_recipefoods.rb
+++ db/migrate/20230704153934_create_recipefoods.rb
@@ -3,7 +3,7 @@ class CreateRecipefoods < ActiveRecord::Migration[7.0]
     create_table :recipefoods do |t|
       t.integer :quantity
       t.references :foods, index: true, foreign_key: true
-      r.references :recipes, index: true, foreign_key: true
+      t.references :recipes, index: true, foreign_key: true
       t.timestamps
     end
   end
